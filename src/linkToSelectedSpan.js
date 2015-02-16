a110d1a008fbb53c77e76c4a3a576baae7fbc557
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-16 15:47:52 +0900

    Use the querySelectorAll instead of jQuery selector.

--- src/linkToSelectedSpan.js
+++ src/linkToSelectedSpan.js
@@ -4,46 +4,47 @@ import SelectionObserver from './SelectionObserver'
 
 main()
 
-function createLinkSpaceContent($target) {
+function createLinkSpaceContent(element) {
     var template = `
         <span class="range"></span>
         <a class="link"></a>
     `
 
-    return $target.append(template)
+    return element.innerHTML = template
 }
 
-function updateLinkSpaceContent($target, select, url) {
+function updateLinkSpaceContent(element, select, url) {
     var template = `<span class="range">${select}</span><a class="link" href="${url}">&lt${url}&gt</a>`
 
-    $target[0].innerHTML = template
+    element.innerHTML = template
 }
 
-function selected($linkSpace, select) {
+function selected(linkSpace, select) {
     var url = pathJoin(location.href, 'spans/' + select)
 
-    updateLinkSpaceContent($linkSpace, select, url)
+    updateLinkSpaceContent(linkSpace, select, url)
 }
 
-function bindEvent($target, $linkSpace) {
+function bindEvent($target, linkSpace) {
     var observer = new SelectionObserver()
 
     $target
         .on('click', observer.handler)
 
-    observer.observable((select) => selected($linkSpace, select))
+    observer.observable((select) => selected(linkSpace, select))
 }
 
 function linkToSelectedSpan(selector) {
-    var $linkSpace = jQuery(selector)
+    var $target = this,
+        linkSpaces = document.querySelectorAll(selector)
 
-    if ($linkSpace.length === 0) {
+    if (linkSpaces.length === 0) {
         console.warn('no element is found. selector: ', selector)
         return
     }
 
-    createLinkSpaceContent($linkSpace)
-    bindEvent(this, $linkSpace)
+    createLinkSpaceContent(linkSpaces[0])
+    bindEvent($target, linkSpaces[0])
 
     console.log('start to observe element.')
     return this
