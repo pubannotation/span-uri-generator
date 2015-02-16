8f80131f8ae5edbf92bd2bc40ef117c1da4ecd5f
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-16 15:38:35 +0900

    Extract a module 'SelectionObserver'.

---
+++ src/SelectionObserver.js
@@ -0,0 +1,76 @@
+import observ from 'observ'
+
+export default function() {
+    var o = observ()
+
+    return {
+        observable: o,
+        handler: function triggerSelect(event) {
+            var selection = window.getSelection()
+
+            if (selection.isCollapsed) return
+            if (selection.anchorNode.nodeName !== '#text') return
+            if (selection.focusNode.nodeName !== '#text') return
+
+            o.set(toSelectString(getSelectedPosition(selection)))
+        }
+    }
+}
+
+function toLength(node) {
+    return node.nodeName === "#text" ? node.length :
+        node.nodeName === "BR" ? 1 :
+        0
+}
+
+function filterPrevNode(selfNode, resultArray, node, index) {
+    if (node === selfNode)
+        resultArray.isSelfNodeFound = true
+
+    if (!resultArray.isSelfNodeFound)
+        resultArray.push(node)
+
+    return resultArray
+}
+
+function getPrevNodesOffset(node) {
+    var brotherNodes = Array.prototype.slice.apply(node.parentElement.childNodes),
+        pos = brotherNodes
+        .reduce((prevValue, currentValue, i) => filterPrevNode(node, prevValue, currentValue, i), [])
+        .reduce((pos, n) => pos += toLength(n), 0)
+
+    return pos
+}
+
+function getPosition(selection, name) {
+    return getPrevNodesOffset(selection[name + 'Node']) +
+        selection[name + 'Offset']
+}
+
+function toBeginEnd(apos, fpos) {
+    return {
+        begin: apos < fpos ? apos : fpos,
+        end: apos < fpos ? fpos : apos
+    }
+}
+
+function getSelectedPosition(selection) {
+    var apos = getPosition(selection, 'anchor'),
+        fpos = getPosition(selection, 'focus')
+
+    return toBeginEnd(apos, fpos)
+}
+
+function toSelectString(select) {
+    return select.begin + '-' + select.end
+}
+
+function triggerSelect(observ, event) {
+    var selection = window.getSelection()
+
+    if (selection.isCollapsed) return
+    if (selection.anchorNode.nodeName !== '#text') return
+    if (selection.focusNode.nodeName !== '#text') return
+
+    observ.set(toSelectString(getSelectedPosition(selection)))
+}
--- src/linkToSelectedSpan.js
+++ src/linkToSelectedSpan.js
@@ -1,65 +1,8 @@
 import observ from 'observ'
 import pathJoin from './pathJoin'
+import SelectionObserver from './SelectionObserver'
 
 main()
-
-function toLength(node) {
-    return node.nodeName === "#text" ? node.length :
-        node.nodeName === "BR" ? 1 :
-        0
-}
-
-function filterPrevNode(selfNode, resultArray, node, index) {
-    if (node === selfNode)
-        resultArray.isSelfNodeFound = true
-
-    if (!resultArray.isSelfNodeFound)
-        resultArray.push(node)
-
-    return resultArray
-}
-
-function getPrevNodesOffset(node) {
-    var brotherNodes = Array.prototype.slice.apply(node.parentElement.childNodes),
-        pos = brotherNodes
-        .reduce((prevValue, currentValue, i) => filterPrevNode(node, prevValue, currentValue, i), [])
-        .reduce((pos, n) => pos += toLength(n), 0)
-
-    return pos
-}
-
-function getPosition(selection, name) {
-    return getPrevNodesOffset(selection[name + 'Node']) +
-        selection[name + 'Offset']
-}
-
-function toBeginEnd(apos, fpos) {
-    return {
-        begin: apos < fpos ? apos : fpos,
-        end: apos < fpos ? fpos : apos
-    }
-}
-
-function getSelectedPosition(selection) {
-    var apos = getPosition(selection, 'anchor'),
-        fpos = getPosition(selection, 'focus')
-
-    return toBeginEnd(apos, fpos)
-}
-
-function toSelectString(select) {
-    return select.begin + '-' + select.end
-}
-
-function triggerSelect(observ, event) {
-    var selection = window.getSelection()
-
-    if (selection.isCollapsed) return
-    if (selection.anchorNode.nodeName !== '#text') return
-    if (selection.focusNode.nodeName !== '#text') return
-
-    observ.set(toSelectString(getSelectedPosition(selection)))
-}
 
 function createLinkSpaceContent($target) {
     var template = `
@@ -78,20 +21,22 @@ function updateLinkSpaceContent($target, select, url) {
 
 function selected($linkSpace, select) {
     var url = pathJoin(location.href, 'spans/' + select)
+
     updateLinkSpaceContent($linkSpace, select, url)
 }
 
 function bindEvent($target, $linkSpace) {
-    var select = observ('');
+    var observer = new SelectionObserver()
 
     $target
-        .on('click', (event) => triggerSelect(select, event))
+        .on('click', observer.handler)
 
-    select((select) => selected($linkSpace, select))
+    observer.observable((select) => selected($linkSpace, select))
 }
 
 function linkToSelectedSpan(selector) {
     var $linkSpace = jQuery(selector)
+
     if ($linkSpace.length === 0) {
         console.warn('no element is found. selector: ', selector)
         return
