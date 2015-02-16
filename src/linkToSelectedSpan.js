eb450dcd3a55daf3dc34614515e7fc3240f4ef7a
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-16 15:12:08 +0900

    Remove unnecessary semi collon.

--- src/linkToSelectedSpan.js
+++ src/linkToSelectedSpan.js
@@ -1,54 +1,64 @@
-import pathJoin from './pathJoin';
+import pathJoin from './pathJoin'
 
-main();
+main()
 
 function toLength(node) {
     return node.nodeName === "#text" ? node.length :
         node.nodeName === "BR" ? 1 :
-        0;
+        0
+}
+
+function filterPrevNode(selfNode, resultArray, node, index) {
+    if (node === selfNode)
+        resultArray.isSelfNodeFound = true
+
+    if (!resultArray.isSelfNodeFound)
+        resultArray.push(node)
+
+    return resultArray
 }
 
 function getPrevNodesOffset(node) {
-    var pos = 0,
-        brotherNodes = node.parentElement.childNodes;
-    for (var i = 0; brotherNodes[i] !== node; i++) {
-        pos += toLength(brotherNodes[i]);
-    }
-    return pos;
+    var brotherNodes = Array.prototype.slice.apply(node.parentElement.childNodes),
+        pos = brotherNodes
+        .reduce((prevValue, currentValue, i) => filterPrevNode(node, prevValue, currentValue, i), [])
+        .reduce((pos, n) => pos += toLength(n), 0)
+
+    return pos
 }
 
 function getPosition(selection, name) {
     return getPrevNodesOffset(selection[name + 'Node']) +
-        selection[name + 'Offset'];
+        selection[name + 'Offset']
 }
 
 function toBeginEnd(apos, fpos) {
     return {
         begin: apos < fpos ? apos : fpos,
         end: apos < fpos ? fpos : apos
-    };
+    }
 }
 
 function getSelectedPosition(selection) {
     var apos = getPosition(selection, 'anchor'),
-        fpos = getPosition(selection, 'focus');
+        fpos = getPosition(selection, 'focus')
 
-    return toBeginEnd(apos, fpos);
+    return toBeginEnd(apos, fpos)
 }
 
 function toSelectString(select) {
-    return select.begin + '-' + select.end;
+    return select.begin + '-' + select.end
 }
 
 function triggerSelect(event) {
-    var selection = window.getSelection();
+    var selection = window.getSelection()
 
-    if (selection.isCollapsed) return;
-    if (selection.anchorNode.nodeName !== '#text') return;
-    if (selection.focusNode.nodeName !== '#text') return;
+    if (selection.isCollapsed) return
+    if (selection.anchorNode.nodeName !== '#text') return
+    if (selection.focusNode.nodeName !== '#text') return
 
     jQuery(event.target)
-        .trigger('select', toSelectString(getSelectedPosition(selection)));
+        .trigger('select', toSelectString(getSelectedPosition(selection)))
 }
 
 function createLinkSpaceContent($target) {
@@ -61,36 +71,36 @@ function createLinkSpaceContent($target) {
 }
 
 function updateLinkSpaceContent($target, select, url) {
-    var template = `<span class="range">${select}</span><a class="link" href="${url}">&lt;${url}&gt;</a>`;
+    var template = `<span class="range">${select}</span><a class="link" href="${url}">&lt${url}&gt</a>`
 
-    $target[0].innerHTML = template;
+    $target[0].innerHTML = template
 }
 
 function selected($linkSpace, event, select) {
-    var url = pathJoin(location.href, 'spans/' + select);
-    updateLinkSpaceContent($linkSpace, select, url);
+    var url = pathJoin(location.href, 'spans/' + select)
+    updateLinkSpaceContent($linkSpace, select, url)
 }
 
 function bindEvent($target, $linkSpace) {
     $target
         .on('click', triggerSelect)
-        .on('select', (event, select) => selected($linkSpace, event, select));
+        .on('select', (event, select) => selected($linkSpace, event, select))
 }
 
 function linkToSelectedSpan(selector) {
-    var $linkSpace = jQuery(selector);
+    var $linkSpace = jQuery(selector)
     if ($linkSpace.length === 0) {
-        console.warn('no element is found. selector: ', selector);
-        return;
+        console.warn('no element is found. selector: ', selector)
+        return
     }
 
-    createLinkSpaceContent($linkSpace);
-    bindEvent(this, $linkSpace);
+    createLinkSpaceContent($linkSpace)
+    bindEvent(this, $linkSpace)
 
-    console.log('start to observe element.');
-    return this;
+    console.log('start to observe element.')
+    return this
 }
 
 function main() {
-    jQuery.fn.linkToSelectedSpan = linkToSelectedSpan;
+    jQuery.fn.linkToSelectedSpan = linkToSelectedSpan
 }
