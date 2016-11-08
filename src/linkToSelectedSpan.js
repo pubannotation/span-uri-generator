9b20a02a5b0181c55e542167c8815c3cc422c868
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-11-08 16:15:03 +0900

    Don’t init the link when it is clicked

--- src/linkToSelectedSpan.js
+++ src/linkToSelectedSpan.js
@@ -6,59 +6,67 @@ import SelectionObserver from './SelectionObserver'
 main()
 
 function initLinkSpaceContent(element) {
-    var template = `
+  var template = `
         <span class="message">
             Select a part of text above to get its span-url.
         </span>`
 
-    element.innerHTML = template
+  element.innerHTML = template
 }
 
 function updateLinkSpaceContent(element, select, url) {
-    var template = `<a class="link" href="${url}">&lt${url}&gt</a> selected`
+  var template = `<a class="link span-uri-generator" href="${url}">&lt${url}&gt</a> selected`
 
-    element.innerHTML = template
+  element.innerHTML = template
 }
 
 function toSelectString(select) {
-    return select.begin + '-' + select.end
+  return select.begin + '-' + select.end
 }
 
 function selected(linkSpace, select) {
-    var parsed = url.parse(location.href),
-        selectString = toSelectString(select),
-        pathname = parsed.pathname.replace(/spans\/\d+-\d+/, '')
+  var parsed = url.parse(location.href),
+    selectString = toSelectString(select),
+    pathname = parsed.pathname.replace(/spans\/\d+-\d+/, '')
 
-    parsed.pathname = pathJoin(pathname, `spans/${selectString}`)
+  parsed.pathname = pathJoin(pathname, `spans/${selectString}`)
 
-    updateLinkSpaceContent(linkSpace, selectString, url.format(parsed))
+  updateLinkSpaceContent(linkSpace, selectString, url.format(parsed))
 }
 
 function bindEvent($target, linkSpace) {
-    var observer = new SelectionObserver()
+  var observer = new SelectionObserver()
 
-    $target.on('click', observer.handler)
-    $(document.body).on('mousedown', () => initLinkSpaceContent(linkSpace))
+  $target.on('click', observer.handler)
+  $(document.body)
+    .on('mousedown', (e) => {
+      // Do nothing when click the link.
+      if (e.target.classList.contains('span-uri-generator')) {
+        return
+      }
 
-    observer.observable((select) => selected(linkSpace, select))
+      initLinkSpaceContent(linkSpace)
+    })
+
+  observer.observable((select) => selected(linkSpace, select))
 }
 
 function linkToSelectedSpan(selector) {
-    var $target = this,
-        linkSpace = document.querySelector(selector)
+  var $target = this,
+    linkSpace = document.querySelector(selector)
 
-    if (!linkSpace) {
-        console.warn('no element is found. selector: ', selector)
-        return
-    }
+  if (!linkSpace) {
+    console.warn('no element is found. selector: ', selector)
+    return
+  }
 
-    initLinkSpaceContent(linkSpace)
-    bindEvent($target, linkSpace)
+  initLinkSpaceContent(linkSpace)
+  bindEvent($target, linkSpace)
 
-    console.log('start to observe element.')
-    return this
+  console.log('start to observe element.')
+  return this
 }
 
 function main() {
-    jQuery.fn.linkToSelectedSpan = linkToSelectedSpan
+  jQuery.fn.linkToSelectedSpan = linkToSelectedSpan
 }
